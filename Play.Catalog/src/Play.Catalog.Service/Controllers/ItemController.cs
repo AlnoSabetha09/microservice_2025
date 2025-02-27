0f675fbfb5eab14fb9d22c3954bcfb3ea8245665
unknown <ireneus.alno@si.ukdw.ac.id>
2025-02-28 03:42:58 +0700

    add crud

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemController.cs
@@ -1,32 +1,82 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Play.Catalog.Service.dto;
 
 namespace Play.Catalog.Service.Controllers
 {
-    [Route("[controller]")]
-    public class ItemController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ItemController : ControllerBase
     {
-        private readonly ILogger<ItemController> _logger;
-
-        public ItemController(ILogger<ItemController> logger)
+        private static readonly List<ItemDto> items = new()
         {
-            _logger = logger;
+            new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Bronze sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow)
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
+        {
+            return items;
         }
 
-        public IActionResult Index()
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetById(Guid id)
         {
-            return View();
+           var item = items.Where(item => item.Id == id).SingleOrDefault();
+           if (item is null)
+           {
+               return NotFound();
+           }
+           return Ok(item);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
         {
-            return View("Error!");
+            var newItem = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(newItem);
+            return CreatedAtAction(nameof(GetById), new { id = newItem.Id }, newItem);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            var updatedItem = existingItem with
+            {
+                Name = updateItemDto.Name,
+                Description = updateItemDto.Description,
+                Price = updateItemDto.Price
+            };
+
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            items[index] = updatedItem;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            items.Remove(existingItem);
+
+            return NoContent();
         }
     }
 }
